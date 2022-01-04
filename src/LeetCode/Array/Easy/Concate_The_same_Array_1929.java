98de8d4c9f2cc979a8524308dbf719e050d98b1f
thiyagesh kanna <thiyagesh14092000@gmail.com>
2022-01-04 15:44:51 +0530

    Concate_The_same_Array_1929

---
+++ src/LeetCode/Array/Easy/Concate_The_same_Array_1929.java
@@ -0,0 +1,30 @@
+package LeetCode.Array.Easy;
+
+// https://leetcode.com/problems/concatenation-of-array/
+
+import java.util.Arrays;
+
+public class Concate_The_same_Array_1929 {
+    public static void main(String[] args) {
+        int[] nums={1,2,1};
+        int[] ans=concateArray(nums);
+        System.out.println(Arrays.toString(ans));
+    }
+
+    private static int[] concateArray(int[] nums) {
+        int length=nums.length *2;
+        int[] newArray=new int[length];
+        int position=0;
+        for(int i=0;i<nums.length;i++){
+            newArray[position]=nums[i];
+            position++;
+        }
+        for(int i=0;i<nums.length;i++){
+            newArray[position]=nums[i];
+            position++;
+        }
+
+
+        return newArray;
+    }
+}
