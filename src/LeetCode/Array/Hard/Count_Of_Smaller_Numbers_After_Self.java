0f00639669f378d81d4408b40f235b9e425f9404
thiyagesh kanna <thiyagesh14092000@gmail.com>
2022-01-10 21:01:36 +0530

    Count_Of_Smaller_Numbers_After_Self.+ It has to be optimized.

---
+++ src/LeetCode/Array/Hard/Count_Of_Smaller_Numbers_After_Self.java
@@ -0,0 +1,28 @@
+package LeetCode.Array.Hard;
+//https://leetcode.com/problems/count-of-smaller-numbers-after-self/
+
+//This answer is correct. It passed 58 test cases balance test case not passed due to time limit exit.
+import java.util.ArrayList;
+import java.util.List;
+
+public class Count_Of_Smaller_Numbers_After_Self {
+    public static void main(String[] args) {
+        int[] nums={-1,-2};
+        List<Integer> as=new ArrayList<Integer>();
+        as=countSmaller(nums);
+        System.out.println(as);
+    }
+    static List<Integer> countSmaller(int[] nums) {
+        List<Integer> as=new ArrayList<Integer>();
+        for(int i=0;i<nums.length;i++){
+            int count=0;
+            for(int j=i+1;j< nums.length;j++){
+                if(nums[i]>nums[j]){
+                    count++;
+                }
+            }
+            as.add(count);
+        }
+        return as;
+    }
+}
