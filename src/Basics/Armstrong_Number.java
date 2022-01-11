102199014341b736245dcad3d9974578ec6cf9df
thiyagesh kanna <thiyagesh14092000@gmail.com>
2022-01-11 13:19:11 +0530

    Check_Armstring_Number_For_Any_range_Not_Upto_1000...Ufff

---
+++ src/Basics/Armstrong_Number.java
@@ -0,0 +1,25 @@
+package Basics;
+//Armstrong number
+import java.util.Scanner;
+
+public class Armstrong_Number {
+    public static void main(String[] args) {
+        Scanner scn=new Scanner(System.in);
+        int number=scn.nextInt();
+        int duplicate=number;
+        int finalAnswer=0;
+        String dupi= String.valueOf(number);
+        int lenght=dupi.length();
+        while (number>0){
+            int remainder=number%10;
+            finalAnswer=(int) (finalAnswer+(Math.pow(remainder,lenght)));
+            number/=10;
+        }
+        if(finalAnswer==duplicate){
+            System.out.println(duplicate+" is a Armstrong Number ");
+        }
+        else{
+            System.out.println(duplicate+" is not a Armstrong Number ");
+        }
+    }
+}
