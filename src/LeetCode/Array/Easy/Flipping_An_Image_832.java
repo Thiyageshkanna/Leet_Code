a746aca9785a6edaceeb374dc29093676a9fe57f
thiyagesh kanna <thiyagesh14092000@gmail.com>
2022-01-15 00:07:59 +0530

    Find_The_Highest_Altitude_1732

---
+++ src/LeetCode/Array/Easy/Flipping_An_Image_832.java
@@ -0,0 +1,43 @@
+package LeetCode.Array.Easy;
+
+import java.util.Arrays;
+
+public class Flipping_An_Image_832 {
+    public static void main(String[] args) {
+        int[][] image={
+                {1,1,0},
+                {1,0,1},
+                {0,0,0}
+        };
+        int[][] ans=flipAndInvertImage(image);
+        System.out.println(Arrays.deepToString(ans));
+    }
+    static int[][] flipAndInvertImage(int[][] image) {
+
+        // Swapping with two pointers
+        for (int i=0;i< image.length;i++){
+            int start=0;
+            int end=image.length-1;
+            while(start<end){
+                int temp=image[i][start];
+                image[i][start]=image[i][end];
+                image[i][end]=temp;
+                start++;
+                end--;
+            }
+        }
+        //Inverting the image
+        for(int i=0;i<image.length;i++){
+            for(int j=0;j<image.length;j++){
+                if(image[i][j]==0){
+                    image[i][j]=1;
+                }
+                else{
+                    image[i][j]=0;
+                }
+            }
+        }
+        return image;
+    }
+
+}
