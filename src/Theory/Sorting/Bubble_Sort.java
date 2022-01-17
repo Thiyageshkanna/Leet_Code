090c795e124a16f3fbc4a746703f323bd4ca3a6d
thiyagesh kanna <thiyagesh14092000@gmail.com>
2022-01-17 19:49:50 +0530

    Bubble_Sort

---
+++ src/Theory/Sorting/Bubble_Sort.java
@@ -0,0 +1,29 @@
+package Theory.Sorting;
+
+import java.util.Arrays;
+
+public class Bubble_Sort {
+    public static void main(String[] args) {
+        int[] nums={2,4,1,5,3};
+        bubbleSort(nums);
+        System.out.println(Arrays.toString(nums));
+    }
+
+    private static void bubbleSort(int[] nums) {
+        boolean isSwapped=false;
+        for(int i=0;i<nums.length;i++){
+            for (int j = 1; j <nums.length-i ; j++) {
+                if(nums[j]<nums[j-1]){
+                    int temp=nums[j];
+                    nums[j]=nums[j-1];
+                    nums[j-1]=temp;
+                    isSwapped=true;
+                }
+            }
+            // When the input is already in best case ,we don't need to go for counting (i) so we are breaking
+            if(!isSwapped){
+                break;
+            }
+        }
+    }
+}
