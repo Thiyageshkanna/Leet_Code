c5a5c276f04ac3abd3c849e6878927459487c0c4
thiyagesh kanna <thiyagesh14092000@gmail.com>
2022-01-23 22:38:30 +0530

    Determine_Whether_Matrix_Can_be_Obtained_by_Rotation_1886

---
+++ src/LeetCode/Array/Easy/Determine_Whether_Matrix_Can_be_Obtained_by_Rotation_1886.java
@@ -0,0 +1,53 @@
+package LeetCode.Array.Easy;
+
+import java.util.Arrays;
+
+public class Determine_Whether_Matrix_Can_be_Obtained_by_Rotation_1886 {
+    public static void main(String[] args) {
+        int[][] maxtrix={
+                {0,1},
+                {1,0}
+        };
+        int[][] target={
+                {1,0},
+                {0,1}
+        };
+        boolean ans=findRotation(maxtrix,target);
+        System.out.println(ans);
+    }
+    static boolean findRotation(int[][] mat, int[][] target) {
+        for (int i = 0; i <4 ; i++) {
+            if(same(mat,target)){
+                return true;
+            }
+            mat=rotate(mat);
+        }
+        return false;
+    }
+
+    private static boolean same(int[][] mat, int[][] target) {
+        boolean isSame=true;
+        if(mat.length ==target.length && mat[0].length==target[0].length){
+            for(int i=0;i<mat.length;i++){
+                for (int j = 0; j < mat[0].length; j++) {
+                    if(target[i][j]!=mat[i][j]){
+                        return isSame=false;
+                    }
+                }
+            }
+        }
+        return isSame;
+    }
+
+    static int[][] rotate(int[][]mat){
+        int rows=mat.length;
+        int columns=mat[0].length;
+        int[][] ans=new int[columns][rows];
+        for(int i=0;i<rows;i++){
+            for (int j = 0; j < columns ; j++) {
+                ans[j][rows-i-1]=mat[i][j];
+            }
+        }
+        return ans;
+    }
+}
