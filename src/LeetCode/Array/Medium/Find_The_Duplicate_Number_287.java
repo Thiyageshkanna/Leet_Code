ae5bef5b3a2de82e8031d1e661c7edd5b4494ee7
thiyagesh kanna <thiyagesh14092000@gmail.com>
2022-01-26 00:29:07 +0530

    Find_The_Duplicate_Number_287.java

---
+++ src/LeetCode/Array/Medium/Find_The_Duplicate_Number_287.java
@@ -0,0 +1,36 @@
+package LeetCode.Array.Medium;
+
+import java.util.Arrays;
+
+public class Find_The_Duplicate_Number_287 {
+    public static void main(String[] args) {
+        int[] nums={3,1,3,4,2};
+        System.out.println(findDuplicate(nums));
+    }
+    static int findDuplicate(int[] nums) {
+        int i=0;
+        while (i< nums.length){
+            int check=nums[i]-1;
+            if(nums[i]!=nums[check]){
+                swap(nums,i,check);
+            }
+            else{
+                i++;
+            }
+        }
+        int answer=-1;
+        for (int j = 0; j < nums.length; j++) {
+            if(j+1!=nums[j]){
+                answer=nums[j];
+                return answer;
+            }
+        }
+        return answer;
+    }
+
+    private static void swap(int[] nums, int i, int check) {
+        int temp=nums[i];
+        nums[i]=nums[check];
+        nums[check]=temp;
+    }
+}
