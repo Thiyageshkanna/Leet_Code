8d1cdb75fad534635f0a4e2f3cfe55c0a4682e50
thiyagesh kanna <thiyagesh14092000@gmail.com>
2022-01-25 13:22:52 +0530

    Missing_Number_268

---
+++ src/LeetCode/Array/Easy/Missing_Number_268.java
@@ -0,0 +1,30 @@
+package LeetCode.Array.Easy;
+
+public class Missing_Number_268 {
+    public static void main(String[] args) {
+        int[] nums={3,0,1};
+        System.out.println(missingNumber(nums));
+    }
+    static int missingNumber(int[] nums) {
+        int i = 0;
+        while (i < nums.length ) {
+            int check = nums[i];
+            if (nums[i]<nums.length && nums[i] != nums[check]) {
+                swap(nums, i, check);
+            } else {
+                i++;
+            }
+        }
+        for (int j = 0; j < nums.length; j++) {
+            if(nums[j]!=j){
+                return j;
+            }
+        }
+        return nums.length+1;
+    }
+    private static void swap(int[] nums, int i, int check) {
+        int temp=nums[i];
+        nums[i]=nums[check];
+        nums[check]=temp;
+    }
+}
