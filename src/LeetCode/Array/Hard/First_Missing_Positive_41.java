27001ad1ca1b7b693ef88da79cc66de7846ef552
thiyagesh kanna <thiyagesh14092000@gmail.com>
2022-01-31 19:17:03 +0530

    First_Missing_Positive_41.java

--- src/LeetCode/Array/Hard/First_Missing_Positive_41.java
+++ src/LeetCode/Array/Hard/First_Missing_Positive_41.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 //https://leetcode.com/problems/first-missing-positive/
 public class First_Missing_Positive_41 {
     public static void main(String[] args) {
-        int[] nums={-1};
+        int[] nums={7,8,9,11,12};
         System.out.println(firstMissingPositive(nums));
     }
     static int firstMissingPositive(int[] nums) {
@@ -13,7 +13,7 @@ public class First_Missing_Positive_41 {
            while (i<nums.length){
                int check=nums[i]-1;
                //here comes the main part ignore negative numbers and the numbers where index are greater than the nums.length
-               if(nums[i]>0 && nums[i]< nums.length && nums[i]!=nums[check]){
+               if(nums[i]>0 && nums[i]<nums.length  && nums[i]!=nums[check]){
                    swap(nums,i,check);
                }
                else{
