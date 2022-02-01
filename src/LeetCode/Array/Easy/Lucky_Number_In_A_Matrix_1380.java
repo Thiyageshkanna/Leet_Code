764c04e4b9750c62206af48402ef1821a926cb54
thiyagesh kanna <thiyagesh14092000@gmail.com>
2022-02-01 14:43:04 +0530

    Lucky_Number_In_A_Matrix_1380.java

--- src/LeetCode/Array/Easy/Lucky_Number_In_A_Matrix_1380.java
+++ src/LeetCode/Array/Easy/Lucky_Number_In_A_Matrix_1380.java
@@ -21,19 +21,25 @@ public class Lucky_Number_In_A_Matrix_1380 {
         int minColumnIndex = 0;
 
         for (int i = 0; i < matrix.length; i++) {
+            // i th loop is common for both
             int max=Integer.MIN_VALUE;
             int min=Integer.MAX_VALUE;
+            //Finding smallest number in the row
             for (int j = 0; j < matrix[0].length; j++) {
+
                 if(matrix[i][j]<min){
                     min=matrix[i][j];
                     minColumnIndex=j;
                 }
             }
+            //finding greatest number in the minColumnIndex
+            // It only checks smallest row'th index column and return its maximum
             for (int j = 0; j < matrix.length; j++) {
                 if(matrix[j][minColumnIndex]>max){
                     max=matrix[j][minColumnIndex];
                 }
             }
+            //when both are fell in same place which is maximum column and minimum row add it to the arraylist
             if(min==max){
                 al.add(min);
             }
