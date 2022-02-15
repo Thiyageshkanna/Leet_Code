6aa2fb2eafde67721e3f936cf694d7af2164d919
thiyagesh kanna <thiyagesh14092000@gmail.com>
2022-02-15 20:01:39 +0530

    Jump_Game_55

---
+++ src/LeetCode/Array/Medium/Jump_Game_55.java
@@ -0,0 +1,20 @@
+package LeetCode.Array.Medium;
+//https://leetcode.com/problems/jump-game/
+public class Jump_Game_55 {
+    public static void main(String[] args) {
+        int[] nums={3,2,1,0,4};
+        System.out.println(canJump(nums));
+    }
+    static boolean canJump(int[] nums) {
+        int reached=0;
+        boolean isReached=true;
+        //We are checking for false, if it reached is less than i, return false
+        for (int i = 0; i < nums.length; i++) {
+            if(reached<i){
+                return false;
+            }
+            reached=Math.max(reached,i+nums[i]);
+        }
+        return true;
+    }
+}
