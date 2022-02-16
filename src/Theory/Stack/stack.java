cfa22cc2b865632e2817ca3a10ff6bf9b70e3ea8
thiyagesh kanna <thiyagesh14092000@gmail.com>
2022-02-16 23:02:07 +0530

    stack

--- src/Theory/Stack/stack.java
+++ src/Theory/Stack/stack.java
@@ -2,10 +2,12 @@ package Theory.Stack;
 
 public class stack {
     public static void main(String[] args) {
+
         String name="Thiyagesh";
         String name2=name;
         name="no name";
         System.out.println(name2);
         System.out.println(name);
+
     }
 }
