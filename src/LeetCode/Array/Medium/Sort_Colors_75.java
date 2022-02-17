d7514d4c20a589d0f0e023b98503dd1fad6a96d6
thiyagesh kanna <thiyagesh14092000@gmail.com>
2022-02-17 21:45:28 +0530

    Build_Array_From_permutation_1920

--- src/LeetCode/Array/Medium/Sort_Colors_75.java
+++ src/LeetCode/Array/Medium/Sort_Colors_75.java
@@ -5,10 +5,45 @@ import java.util.Arrays;
 public class Sort_Colors_75 {
     public static void main(String[] args) {
         int[] nums={2,0,2,1,1,0};
-        sortColors(nums);
+        //bubbleSortColors(nums);
     }
+    //IG this is the most optimal approach for the question
+
+    static void cycleSortOrCountingSort(int[] nums){
+        int red=0;
+        int white=0;
+        int blue=0;
+        for (int i = 0; i < nums.length; i++) {
+            if(nums[i]==0){
+                red++;
+            }
+            else if(nums[i]==1){
+                white++;
+            }
+            else if(nums[i]==2){
+                blue++;
+            }
+        }
+        for (int i = 0; i < nums.length; i++) {
+            if (i<red){
+                nums[i]=0;
+            }
+            else if(i<(red+white)){
+                nums[i]=1;
+            }
+            else{
+                nums[i]=2;
+            }
+        }
+        System.out.println(Arrays.toString(nums));
+    }
+
+
+
+
+
     //by using bubble sort method just a brute force approach
-    static void sortColors(int[] nums) {
+    static void bubbleSortColors(int[] nums) {
         boolean isSorted=false;
         for (int i = 0; i < nums.length; i++) {
             for (int j = 1; j < nums.length-i; j++) {
