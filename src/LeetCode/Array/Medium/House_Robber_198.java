80ad794300a21fa2aa3f42a3da3c8c3b96c9c53f
thiyagesh kanna <thiyagesh14092000@gmail.com>
2022-02-18 21:12:02 +0530

    House_Robber_198

---
+++ src/LeetCode/Array/Medium/House_Robber_198.java
@@ -0,0 +1,23 @@
+package LeetCode.Array.Medium;
+
+public class House_Robber_198 {
+    public static void main(String[] args) {
+        int[] nums={1,2};
+        System.out.println(rob(nums));
+    }
+    static int rob(int[] nums) {
+        int i=0;
+        int ans=0;
+        int j=1;
+        while (i<nums.length){
+            ans+=nums[i];
+                i+=2;
+        }
+        int ans1=0;
+        while (j<nums.length){
+            ans1+=nums[j];
+            j+=2;
+        }
+        return Math.max(ans,ans1);
+    }
+}
