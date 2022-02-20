ecf4e151027f4f8d1732ff712b2ab7de58a7d98a
thiyagesh kanna <thiyagesh14092000@gmail.com>
2022-02-20 23:35:53 +0530

    Guess_Number_higher_Or_Lower_374

---
+++ src/LeetCode/Binary_Search/Easy/Guess_Number_higher_Or_Lower_374.java
@@ -0,0 +1,28 @@
+package LeetCode.Binary_Search.Easy;
+//https://leetcode.com/problems/guess-number-higher-or-lower/
+public class Guess_Number_higher_Or_Lower_374 {
+    public static void main(String[] args) {
+        System.out.println("There are some inbuilt methods so we are just commenting the code");
+
+        /*
+         public int guessNumber(int n) {
+       int start=1;
+        int end=n;
+        //here it comes equals because it is number not an array => nums.length
+        while(start<=end){
+            int mid=start+(end-start)/2;
+            if(guess(mid)==0){
+                return mid;
+            }
+            if(guess(mid)==1){
+                start=mid+1;
+            }
+            else{
+                end=mid-1;
+            }
+        }
+        return start;
+    }
+         */
+    }
+}
