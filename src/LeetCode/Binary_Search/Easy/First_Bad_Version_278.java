c926ede4014c2506ac1d37d087765d63d010252f
thiyagesh kanna <thiyagesh14092000@gmail.com>
2022-02-21 22:36:49 +0530

    First_Bad_Version_278

--- src/LeetCode/Binary_Search/Easy/First_Bad_Version_278.java
+++ src/LeetCode/Binary_Search/Easy/First_Bad_Version_278.java
@@ -2,6 +2,7 @@ package LeetCode.Binary_Search.Easy;
 //https://leetcode.com/problems/first-bad-version/
 public class First_Bad_Version_278 {
     public static void main(String[] args) {
+
         System.out.println("We cannot write this code, because it contains some predefined methods");
         /*  int start=1;
         int end=n;
