0478e04652be0dd08c6da3ab323c9873348a2669
thiyagesh kanna <thiyagesh14092000@gmail.com>
2022-02-22 15:43:09 +0530

    Half_Star_pattern

---
+++ src/Basics/Patterns/Star_Increasing_Decreasing.java
@@ -0,0 +1,20 @@
+package Basics.Patterns;
+
+public class Star_Increasing_Decreasing {
+    public static void main(String[] args) {
+        int n=5;
+        for (int row = 1; row < 2*n; row++) {
+            int c;
+            if(row>n){
+                 c=2*n-row;
+            }
+            else{
+                c=row;
+            }
+            for (int column = 0; column < c; column++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
