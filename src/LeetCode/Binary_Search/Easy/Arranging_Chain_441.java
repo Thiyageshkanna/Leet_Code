20c805b6b61100d58227171db2fd2b895d3a4457
thiyagesh kanna <thiyagesh14092000@gmail.com>
2022-02-24 17:59:16 +0530

    Arranging_Chain_441

--- src/LeetCode/Binary_Search/Easy/Arranging_Chain_441.java
+++ src/LeetCode/Binary_Search/Easy/Arranging_Chain_441.java
@@ -5,7 +5,25 @@ public class Arranging_Chain_441 {
         int n=8;
         System.out.println(arrangeCoins(n));
     }
+    //Binary Approach
     static int arrangeCoins(int n){
+        long start=0;
+        long end=n;
+        while (start<=end){
+            long mid= start+(end-start)/2;
+            long currSum=(mid*(mid+1))/2;
+            if(currSum==n) return (int)mid;
+            if(n<currSum){
+                end=mid-1;
+            }
+            else{
+                start=mid+1;
+            }
+        }
+        return (int)end;
+    }
+    //This is not a Optimized Approach
+    static int arrangeCoinss(int n){
         int count=1;
         while (n > 0) {
             n=n-count;
