b9e481bdd95cedececb1902adfea38ce4aadcd86
thiyagesh kanna <thiyagesh14092000@gmail.com>
2022-03-02 21:08:22 +0530

    Intersection_Of_Two_Arrays_349

--- src/LeetCode/Binary_Search/Easy/Intersection_Of_Two_Arrays_349.java
+++ src/LeetCode/Binary_Search/Easy/Intersection_Of_Two_Arrays_349.java
@@ -7,9 +7,38 @@ import java.util.Set;
 
 public class Intersection_Of_Two_Arrays_349 {
     public static void main(String[] args) {
-        int[] nums1={1,2,3,4,5,6,7,8};
-        int[] nums2={1};
-        System.out.println(Arrays.toString(usingHash(nums1, nums2)));
+        int[] nums1={1,2,2,1};
+        int[] nums2={2,2};
+        System.out.println(Arrays.toString(usingTwoPointers(nums1, nums2)));
+    }
+    //Two Pointers
+    static int[] usingTwoPointers(int[] nums1,int[] nums2){
+        Set<Integer> set=new HashSet<>();
+        Arrays.sort(nums1);
+        Arrays.sort(nums2);
+        int i=0;
+        int j=0;
+        while (i <nums1.length && j<nums2.length ){
+            if(nums1[i]<nums2[j]){
+                i++;
+            }
+            else if(nums1[i]>nums2[j]){
+                j++;
+            }
+            else{
+                set.add(nums1[i]);//or set.add(nums2[i]) both are same
+                i++;
+                j++;
+            }
+        }
+        int[] ans=new int[set.size()];
+        int position=0;
+        for (Integer nums:set){
+            ans[position]=nums;
+            position++;
+        }
+
+        return ans;
     }
 
     //Hashing Method
