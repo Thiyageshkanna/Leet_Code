83e89a52e11ed94413ed619c4e6bb87591b68400
Thiyageshkanna <thiyagesh14092000@gmail.com>
2022-03-07 21:16:03 +0530

    Fair_Candy_Swap_888

--- src/LeetCode/Binary_Search/Easy/Fair_Candy_Swap_888.java
+++ src/LeetCode/Binary_Search/Easy/Fair_Candy_Swap_888.java
@@ -1,6 +1,8 @@
 package LeetCode.Binary_Search.Easy;
 
 import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
 public class Fair_Candy_Swap_888 {
     public static void main(String[] args) {
@@ -26,6 +28,27 @@ public class Fair_Candy_Swap_888 {
                 if(aliceSizes[i]-bobSizes[j]==diff){
                     return new int[]{aliceSizes[i],bobSizes[j]};
                 }
+            }
+        }
+        return null;
+    }
+    //HashMap
+    static int[] fairCandySwap(int[] aliceSize,int[] bobSizes){
+        int sum1=0;
+        int sum2=0;
+        Set<Integer> hs=new HashSet<>();
+        for (int i = 0; i < aliceSize.length; i++) {
+            sum1+=aliceSize[i];
+            hs.add(aliceSize[i]);
+        }
+        for (int i = 0; i < bobSizes.length; i++) {
+            sum2+=bobSizes[i];
+        }
+        int diff=(sum1-sum2)/2;
+        for (int i = 0; i < bobSizes.length; i++) {
+            int target=bobSizes[i]+diff;
+            if(hs.contains(target)){
+                return new int[]{target,bobSizes[i]};
             }
         }
         return null;
