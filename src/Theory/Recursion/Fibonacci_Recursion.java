72951e042b3130330f02f25dd31cca36d04b5a2f
Thiyageshkanna <thiyagesh14092000@gmail.com>
2022-03-16 22:36:50 +0530

    Fibonacci_Recursion

--- src/Theory/Recursion/Fibonacci_Recursion.java
+++ src/Theory/Recursion/Fibonacci_Recursion.java
@@ -2,7 +2,7 @@ package Theory.Recursion;
 
 public class Fibonacci_Recursion {
     public static void main(String[] args) {
-        int num=4;
+        int num=5;
         System.out.println(fibo(num));
     }
     static int fibo(int num){
