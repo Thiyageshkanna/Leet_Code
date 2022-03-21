55071cc4a147eaadee8aa1de34759d48bc2c49c0
Thiyageshkanna <thiyagesh14092000@gmail.com>
2022-03-21 23:15:33 +0530

    print_N_to_1

---
+++ src/Theory/Recursion/print_N_to_1.java
@@ -0,0 +1,15 @@
+package Theory.Recursion;
+
+public class print_N_to_1 {
+    public static void main(String[] args) {
+        int n=10;
+        print(n);
+    }
+    static void print(int n){
+        if(n==0){
+            return;
+        }
+        System.out.println(n);
+        print(n-1);
+    }
+}
