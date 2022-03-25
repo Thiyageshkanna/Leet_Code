6b32c5bb074226291cb1383a0aee54c121bbe89f
Thiyageshkanna <thiyagesh14092000@gmail.com>
2022-03-25 23:05:15 +0530

    Pascal_Triangle_118

---
+++ src/LeetCode/Array/Easy/Pascal_Triangle_118.java
@@ -0,0 +1,24 @@
+package LeetCode.Array.Easy;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Pascal_Triangle_118 {
+    public static void main(String[] args) {
+        int numRows=5;
+        System.out.println(generate(numRows));
+
+    }
+    static List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> triangle = new ArrayList<List<Integer>>();
+        ArrayList<Integer> row=new ArrayList<Integer>();
+        for (int i = 0; i < numRows; i++) {
+            row.add(0, 1);
+            for (int j = 1; j <row.size()-1 ; j++) {
+                row.set(j,row.get(j)+row.get(j+1));
+            }
+            triangle.add(new ArrayList<Integer>(row));
+        }
+        return triangle;
+    }
+}
