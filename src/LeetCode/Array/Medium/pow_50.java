753c5af9bdb530900ddfdb25594b60bd6c373f23
Thiyageshkanna <thiyagesh14092000@gmail.com>
2022-03-28 22:54:41 +0530

    pow_50

--- src/LeetCode/Array/Medium/pow_50.java
+++ src/LeetCode/Array/Medium/pow_50.java
@@ -3,14 +3,27 @@ package LeetCode.Array.Medium;
 public class pow_50 {
     public static void main(String[] args) {
         double num=2.00000;
-        System.out.println(myPow(num,10));
+        System.out.println(myPow(num,-2));
     }
     static double myPow(double x, int n) {
-            double ans = 1;
-            for (int i = 1; i <= n; i++) {
-                //To Update ans value
-                ans=ans*x;
-            }
-            return ans;
+               double ans=1.0;
+               long num=n;
+               if(num<0){
+                   num=-1 * num;
+               }
+               while (num>0){
+                   if(num%2==1){
+                       ans=ans*x;
+                       num=num-1;
+                   }
+                   else{
+                       x=x*x;
+                       num=num/2;
+                   }
+               }
+               if(n<0){
+                   ans=1/ans;
+               }
+                return ans;
     }
 }
