52de5374e4a8fa3cae86fc1325f025af2376536a
Thiyageshkanna <thiyagesh14092000@gmail.com>
2022-03-30 00:29:58 +0530

    Majority_Element_169_And_GFG_Answer_Too

--- src/LeetCode/Array/Easy/Majority_Element_169.java
+++ src/LeetCode/Array/Easy/Majority_Element_169.java
@@ -3,11 +3,57 @@ package LeetCode.Array.Easy;
 
 public class Majority_Element_169 {
     public static void main(String[] args) {
-        int[] nums={2,2,3,3,3,3,2};
+        int[] nums={2,2,1,1,1,2,2};
         System.out.println(majorityElement(nums));
     }
-
-
+    //Moore's Voting Algorithm
+    //LeetCode Question
+    static int majorityElement(int[] nums) {
+        int whichElement=0;
+        int count=1;
+        for (int i = 1; i < nums.length; i++) {
+            if(nums[whichElement]==nums[i]){
+                count++;
+            }
+            else{
+                count--;
+            }
+            if(count==0){
+                whichElement=i;
+                count=1;
+            }
+        }
+        return nums[whichElement];
+    }
+    //Moore's Voting Algorithm
+    //GeeksForGeeks Question
+    //https://practice.geeksforgeeks.org/problems/majority-element-1587115620/1#
+    static int majorityElement2(int[] a,int size){
+        int count =1;
+        int res=0;
+        for(int i=1;i<size;i++){
+            if(a[res]==a[i]){
+                count++;
+            }
+            else{
+                count--;
+            }
+            if(count==0){
+                count=1;
+                res=i;
+            }
+        }
+        int finalCount=0;
+        for(int i=0;i<size;i++){
+            if(a[res]==a[i]){
+                finalCount++;
+            }
+        }
+        if(finalCount<=(size)/2){
+            res= -1;
+        }
+        return a[res];
+    }
     //brute force Approach
     static int majorityElement1(int[] nums) {
         if(nums.length==1){
