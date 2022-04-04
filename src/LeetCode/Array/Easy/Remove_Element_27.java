f9e6022e96caf16cff00deed932094664e8ff0e6
Thiyageshkanna <thiyagesh14092000@gmail.com>
2022-04-04 22:59:26 +0530

    Majority_Element_229

---
+++ src/LeetCode/Array/Easy/Remove_Element_27.java
@@ -0,0 +1,23 @@
+package LeetCode.Array.Easy;
+
+import java.util.Arrays;
+
+public class Remove_Element_27 {
+    public static void main(String[] args) {
+        int[] nums={0,1,2,2,3,0,4,2};
+        int val=2;
+        System.out.println(removeElement(nums,val));
+    }
+    static int removeElement(int[] nums, int val) {
+        int begin =0;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]!=val){
+                nums[begin]=nums[i];
+                begin++;
+            }
+        }
+        System.out.println(Arrays.toString(nums));
+        return begin;
+    }
+
+}
