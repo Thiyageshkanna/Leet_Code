58563c147fda1f4116cdcba9a57d10ae274b1639
Thiyageshkanna <thiyagesh14092000@gmail.com>
2022-04-05 17:57:02 +0530

    next_Permutation_31

---
+++ src/LeetCode/Array/Medium/next_Permutation_31.java
@@ -0,0 +1,16 @@
+package LeetCode.Array.Medium;
+
+public class next_Permutation_31 {
+    public static void main(String[] args) {
+        int[] nums={1,2,3};
+        System.out.println(nextPermutation(nums););
+    }
+    static void nextPermutation(int[] nums) {
+        int infectedIndex=0;
+        int n=nums.length-1;
+        //greater than zero because when i=0 (i-1) will go negative value and it will cause ArrayIndexOutfBoundation
+        for (int i = n; i>0 ; i++) {
+
+        }
+    }
+}
