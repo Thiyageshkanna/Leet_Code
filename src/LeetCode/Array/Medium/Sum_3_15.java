aa272a1e1f2f1d57a677d33624de6a7a5c9e88ac
Thiyageshkanna <thiyagesh14092000@gmail.com>
2022-04-18 15:44:04 +0530

    Sum_3_15

--- src/LeetCode/Array/Medium/Sum_3_15.java
+++ src/LeetCode/Array/Medium/Sum_3_15.java
@@ -8,7 +8,35 @@ public class Sum_3_15 {
         int[] nums={-1,0,1,2,-1,-4};
         System.out.println(threeSum(nums));
     }
-    //
+    //Two Pointer Approach Iterative Approach
+    static List<List<Integer>> threeSum(int[] nums){
+        int n=nums.length;
+        if(n<3){
+            return new ArrayList<>();
+        }
+        Arrays.sort(nums);
+        //We Are using set of list not list of list because we need should not store duplicate triplets array
+        Set<List<Integer>> result=new HashSet<>();
+        for (int i = 0; i < n-2; i++) {
+            int j=i+1;
+            int k=n-1;
+            while (j<k){
+                int sum=nums[i]+nums[j]+nums[k];
+                if(sum==0){
+                    result.add(Arrays.asList(nums[i],nums[j],nums[k]));
+                    j++;
+                    k--;
+                }
+                else if(sum<0){
+                    j++;
+                }
+                else {
+                    k--;
+                }
+            }
+        }
+        return new ArrayList(result);
+    }
 
     //Brute Force Approach with time complexity of ==> O(n^3)
     static  List<List<Integer>> threeSum1(int[] nums) {
